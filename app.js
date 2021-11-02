0bed3dfdeeee85b36f2d03b2d08a06c5ccd8080b
izozzz <gabrielgomezph@gmail.com>
2021-11-02 11:54:08 -0300

    Gabo - Primer Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path")
+
+app.use(express.static("public"));
+
+app.listen(3000, ()=>{
+    console.log("Servidor levantado");
+})
+
+app.get("/",(req, res) => {
+    res.sendFile(path.join("C:/Users/Nerox/Desktop/DH Ejercicios/clase 1102/views/home.html"))
+}
+)
